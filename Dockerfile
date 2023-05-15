71db20e710ea49cecc84f24b87a6c373c2a41e49
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-05-15 20:21:02 -0300

    Change DockerFile Commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM amazoncorretto:8-alpine-jdk
-MAINTAINER NGA
-COPY target/nga-0.0.1-SNAPSHOT.jar nga-app.jar
+MAINTAINER ls
+COPY target/ls-0.0.1-SNAPSHOT.jar ls-app.jar
 EXPOSE 808
-ENTRYPOINT  ["java","-jar","nga-app.jar"]
+ENTRYPOINT  ["java","-jar","ls-app.jar"]
--- src/test/java/com/portfolio/nga/NgaApplicationTests.java
+++ src/test/java/com/portfolio/nga/NgaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.nga;
+package com.portfolio.ls;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NgaApplicationTests {
+class lsApplicationTests {
 
 	@Test
 	void contextLoads() {
